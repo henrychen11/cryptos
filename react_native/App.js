4261370dadcc567c124a767654bc786c80bb2b15
jyschwrtz <jyschwrtz@gmail.com>
2017-12-09 15:57:38 -0800

    home & news navigation

--- react_native/App.js
+++ react_native/App.js
@@ -1,44 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-import CoinIndex from './app/components/coin/coin_index';
+import Home from './app/components/screens/home';
+import News from './app/components/screens/news';
 
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <CoinIndex />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const App = StackNavigator({
+  Home: { screen: Home },
+  News: { screen: News }
 });
+
+export default App;
---
+++ react_native/app/components/screens/news.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+
+class News extends React.Component{
+  render() {
+    return(
+      <View>
+        <Text>This is news</Text>
+      </View>
+    );
+  }
+}
+
+export default News;
