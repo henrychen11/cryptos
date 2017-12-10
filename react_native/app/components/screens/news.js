a750a3dddc550f6d93dc61f1d3dd4132c8281a66
henrychen11 <henrychen1@gmail.com>
2017-12-09 18:02:02 -0800

    added news AJAX request

--- react_native/app/components/screens/news.js
+++ react_native/app/components/screens/news.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, ListView } from 'react-native';
 
 
-class News extends React.Component{
+class News extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // dataSource: []
+    };
+  }
+
+  componentDidMount(){
+    const url = 'https://newsapi.org/v2/top-headlines?'
+     + 'sources=google-news&'
+     + 'apiKey=bfa105efac2f4515889f3e14dddfc0f1';
+
+    return fetch(url)
+            .then((response) => (response.json()))
+            .then((responseJSON) => {
+              const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+              this.setState({
+                dataSource: ds.cloneWithRows(responseJSON.articles)
+                });
+              })
+            .then((error) => (console.log(error)));
+
+  }
+  
   render() {
     return(
       <View>
         <Text>This is news</Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData}</Text>}
+          />
       </View>
     );
   }
