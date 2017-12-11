575ff3a7221081ae048f0df5cb82d838f0deb568
Miles McLeod <milesmcleod123@gmail.com>
2017-12-11 00:36:01 -0800

    Get simple express server working and sending data to browser via mongo

--- backend/express_app.js
+++ backend/express_app.js
@@ -1,11 +1,7 @@
-import Express from 'express';
-import Mongoose from 'mongoose';
-import BodyParser from 'body-parser';
-
-const app = Express();
-
-app.use(BodyParser.urlencoded({ extended: true }));
-app.use(BodyParser.json());
+const Express = require('express');
+const Mongoose = require('mongoose');
+const BodyParser = require('body-parser');
+const path = require('path');
 
 const coinSchema = new Mongoose.Schema({
   name: String,
@@ -13,4 +9,43 @@ const coinSchema = new Mongoose.Schema({
   currentValue: Number
 });
 
+const app = Express();
+
 const Coin = Mongoose.model('Coin', coinSchema);
+
+app.use(BodyParser.urlencoded({ extended: true }));
+app.use(BodyParser.json());
+
+Mongoose.Promise = global.Promise;
+
+try {
+  Mongoose.connect('mongodb://localhost/cryptos_coins');
+  console.log('connected to mongoDB');
+} catch (e) {
+  console.log('error connecting to mongo');
+  process.exit(1);
+}
+
+app.get('/coins', (req, res) => {
+  Coin.find((err, coins) => {
+    if (err) return res.status(500).send(err);
+    res.send(coins);
+  });
+});
+
+// app.get('/coins/:coinId', (req, res) => {
+//   Coin.findById((????????????) => {
+//     if (err) return res.status(500).send(err);
+//     res.send(coins);
+//   });
+// });
+
+
+app.use((req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+const server = app.listen('8080', () => {
+  console.log('running server on port ' + server.address().port);
+});
