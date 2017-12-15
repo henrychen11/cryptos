31c7c757aae09ba3512d2eda3166b0bfa99a0380
jyschwrtz <jyschwrtz@gmail.com>
2017-12-14 18:59:37 -0800

    refactor main_chart

--- app/components/charts/main_chart.js
+++ app/components/charts/main_chart.js
@@ -48,7 +48,7 @@ class MainChart extends React.Component {
 
   render() {
       let display;
-      if (this.props.currentChart && this.props.currentChart.symbol) {
+      if (this.props.currentChart) {
 
         const data = this.selectData();
         let values = [];
@@ -57,8 +57,7 @@ class MainChart extends React.Component {
           values.push(dp.value);
           time.push(dp.time);
         });
-      const contentInset = { top: 20, bottom: 20 };
-      if (this.state.chartOption === 'hour'){
+        const contentInset = { top: 20, bottom: 20 };
         display = (
           <View style={styles.chartContainer}>
               <View style={ { flex: 1, height: "100%", flexDirection: 'row' } }>
@@ -98,87 +97,6 @@ class MainChart extends React.Component {
               </View>
           </View>
         );
-      } else if (this.state.chartOption === 'day'){
-        display = (
-          <View style={styles.chartContainer}>
-              <View style={ { flex: 1, height: "100%", flexDirection: 'row' } }>
-                  <View style={ { height: "100%", marginRight: 10 }}>
-                      <YAxis
-                          style={{position: 'absolute', left: 0, bottom: 0, top: 0}}
-                          dataPoints={ data }
-                          contentInset={ contentInset }
-                          labelStyle={ { color: 'white', fontWeight: 'bold' } }
-                          formatLabel={ value => `$${value}` }
-                      />
-                  </View>
-                  <View style={ { flex: 1, height: "100%", flexDirection: 'column' } }>
-                      <LineChart
-                          gridMax={100}
-                          gridMin={-100}
-                          style={ { flex: 1, marginLeft: 25 } }
-                          dataPoints={ data }
-                          svg={{
-                              stroke: 'rgb(134, 65, 244)',
-                          }}
-                          shadowSvg={ {
-                              stroke: 'rgba(134, 65, 244, 0.2)',
-                              strokeWidth: 2,
-                          } }
-                          contentInset={ contentInset }
-                      />
-                  <XAxis
-                      style={ { position: 'absolute', left: 25, bottom: 0, right: 10 } }
-                      values={ data }
-                      formatLabel={ (value, index) => value }
-                      chartType={ XAxis.Type.LINE }
-                      labelStyle={ { color: 'white', fontWeight: 'bold' } }
-                      contentInset={ contentInset }
-                  />
-                  </View>
-              </View>
-          </View>
-        );
-      } else if (this.state.chartOption === 'week'){
-          display = (
-          <View style={styles.chartContainer}>
-              <View style={ { flex: 1, height: "100%", flexDirection: 'row' } }>
-                  <View style={ { height: "100%", marginRight: 10 } }>
-                      <YAxis
-                          style={ { position: 'absolute', left: 0, bottom: 0, top: 0 } }
-                          dataPoints={ data }
-                          contentInset={ contentInset }
-                          labelStyle={ { color: 'white', fontWeight: 'bold' } }
-                          formatLabel={ value => `$${value}` }
-                      />
-                  </View>
-                  <View style={{flex: 1, height: "100%", flexDirection: 'column'}}>
-                      <LineChart
-                          gridMax={100}
-                          gridMin={-100}
-                          style={ { flex: 1, marginLeft: 25} }
-                          dataPoints={ data }
-                          svg={{
-                              stroke: 'rgb(134, 65, 244)',
-                          }}
-                          shadowSvg={ {
-                              stroke: 'rgba(134, 65, 244, 0.2)',
-                              strokeWidth: 2,
-                          } }
-                          contentInset={ contentInset }
-                      />
-                  <XAxis
-                      style={ { position: 'absolute', left: 25, bottom: 0, right: 10 } }
-                      values={ data }
-                      formatLabel={ (value, index) => value }
-                      chartType={ XAxis.Type.LINE }
-                      labelStyle={ { color: 'white', fontWeight: 'bold' } }
-                      contentInset={ contentInset }
-                  />
-                  </View>
-              </View>
-          </View>
-        );
-      }
       }
       return (
           <View style={styles.main}>
