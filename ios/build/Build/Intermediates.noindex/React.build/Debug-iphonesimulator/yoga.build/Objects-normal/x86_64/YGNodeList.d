b60c5aa367e80d87b9ad59fbe6fefc8c2038e3b5
henrychen11 <henrychen1@gmail.com>
2017-12-16 22:07:37 -0800

    added Google Analytics and preped app for release

--- ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/yoga.build/Objects-normal/x86_64/YGNodeList.d
+++ ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/yoga.build/Objects-normal/x86_64/YGNodeList.d
@@ -1,2 +1,6 @@
 dependencies: \
-  /Users/jyschwrtz/AA/flex_project/cryptos/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.c
+  /Users/henrychen/Dropbox/AppAcademy/flex_project/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.c \
+  /Users/henrychen/Dropbox/AppAcademy/flex_project/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.h \
+  /Users/henrychen/Dropbox/AppAcademy/flex_project/node_modules/react-native/ReactCommon/yoga/yoga/YGMacros.h \
+  /Users/henrychen/Dropbox/AppAcademy/flex_project/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.h \
+  /Users/henrychen/Dropbox/AppAcademy/flex_project/node_modules/react-native/ReactCommon/yoga/yoga/YGEnums.h
